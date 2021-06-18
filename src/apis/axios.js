948e4869f186ffed34efcfd8809d47066bdd9a2c
Nithin Dasanakudige <nithindj192@gmail.com>
2021-06-18 11:09:47 +0530

    Moving to https

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -3,5 +3,5 @@ import axios from "axios";
 export const API_KEY = process.env.REACT_APP_OPEN_WEATHER_API_KEY;
 
 export const axiosInstance = axios.create({
-    baseURL: "http://api.openweathermap.org/data/2.5"
+    baseURL: "https://api.openweathermap.org/data/2.5"
 });
